023d41a40bc2dc426fe1fd31012771ff893e849d
Hyohwak Lee <crewe1746@naver.com>
2022-07-19 15:52:32 +0900

    Producer Comsumer Problem - 1

---
+++ section-3/producer-consumer/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	// return error if something went wrong
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	// keep track of which pizza we are making
+	var i = 0
+
+	// run forever or until we receive a quit notification
+	for {
+		// currentPizza := makePizza(i)
+	}
+}
+
+func main() {
+	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// print out a message
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
+
+	// create a producer
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	// run the producer in the background as goroutine
+	go pizzeria(pizzaJob)
+
+	// create and run consumers
+
+	// print out the ending message
+}
