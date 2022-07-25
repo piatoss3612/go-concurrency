d12b8f52d39dc779ae159db45f3f9764e2a383ca
Hyohwak Lee <crewe1746@naver.com>
2022-07-25 10:45:37 +0900

    Sleeping Barber - define variables and barber shop

--- section-5/sleeping-barber/main.go
+++ section-5/sleeping-barber/main.go
@@ -22,16 +22,44 @@
 // semaphores (mutexes) is not needed.
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+var (
+	seatingCapacity = 10
+	arrivalRate     = 100
+	cutDuration     = 1000 * time.Millisecond
+	timeOpen        = 10 * time.Second
+)
 
 func main() {
 	// seed random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HaircutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
 
 	// add barbers
 
@@ -40,5 +68,4 @@ func main() {
 	// add clients
 
 	// block until the barbershop is closed
-
 }
