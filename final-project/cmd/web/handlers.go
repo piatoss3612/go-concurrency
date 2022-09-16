71d45adabbe9cf776247e85d8acc5c79823fef6d
Hyohwak Lee <crewe1746@naver.com>
2022-09-16 10:27:47 +0900

    Subscription service - send an email on invalid log in attempt

--- final-project/cmd/web/handlers.go
+++ final-project/cmd/web/handlers.go
@@ -41,9 +41,14 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !validPassword {
+		msg := Message{
+			To:      email,
+			Subject: "Failed log in attempt",
+			Data:    "Invalid login attempt!",
+		}
+		app.sendEmail(msg)
 		app.Session.Put(r.Context(), "error", "Invalid credentials.")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
-		app.ErrorLog.Println("invalid password")
 		return
 	}
 
