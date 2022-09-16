df3ea7859c02b7545972d4c955cce46405c6658e
Hyohwak Lee <crewe1746@naver.com>
2022-09-16 11:28:30 +0900

    Subscription service - send register verification mail

--- final-project/cmd/web/handlers.go
+++ final-project/cmd/web/handlers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"final-project/data"
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -75,11 +78,46 @@ func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// TODO: validate data
+
 	// create a user
+	u := data.User{
+		Email:     r.Form.Get("email"),
+		FirstName: r.Form.Get("first-name"),
+		LastName:  r.Form.Get("last-name"),
+		Password:  r.Form.Get("password"),
+		Active:    0,
+		IsAdmin:   0,
+	}
+
+	_, err = u.Insert(u)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Unable to create user.")
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
 
 	// send an activation email
+	url := fmt.Sprintf("http://localhost/activate?email=%s", u.Email)
+	signedURL := GenerateTokenFromString(url)
+	app.InfoLog.Println(signedURL)
 
-	// subscribe the user to an account
+	msg := Message{
+		To:       u.Email,
+		Subject:  "Activate your account",
+		Template: "confirmation-email",
+		Data:     template.HTML(signedURL),
+	}
+
+	app.sendEmail(msg)
+
+	app.Session.Put(r.Context(), "flash", "Confirmation email sent. Check your email.")
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
@@ -88,4 +126,6 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	// generate invoice
 
 	// send an email with attachments
+
+	// subscribe the user to an account
 }
