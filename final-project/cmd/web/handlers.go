c0fe994c6d5d13bd2a35ce9081719b49e09f7cd2
Hyohwak Lee <crewe1746@naver.com>
2022-09-16 16:39:25 +0900

    Subscription service - generate manual and send on email

--- final-project/cmd/web/handlers.go
+++ final-project/cmd/web/handlers.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"final-project/data"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/phpdave11/gofpdf"
+	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 )
 
 func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
@@ -213,14 +218,66 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	// generate a menual
+	app.Wait.Add(1)
 
-	// send an email with the manual attached
+	go func() {
+		defer app.Wait.Done()
+
+		pdf := app.generateManual(user, plan)
+		err := pdf.OutputFileAndClose(fmt.Sprintf("./tmp/%d_manual.pdf", user.ID))
+		if err != nil {
+			app.ErrorChan <- err
+			return
+		}
+
+		msg := Message{
+			To:      user.Email,
+			Subject: "Your manual",
+			Data:    "Your user manual is attached",
+			AttachmentMap: map[string]string{
+				"Manual.pdf": fmt.Sprintf("./tmp/%d_manual.pdf", user.ID),
+			},
+		}
+
+		// send an email with the manual attached
+
+		app.sendEmail(msg)
+
+		// test app error chan
+		app.ErrorChan <- errors.New("some custom error")
+	}()
 
 	// subscribe the user to an account
 
 	// redirect
+	app.Session.Put(r.Context(), "flash", "Subscribed!")
+	http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
 }
 
 func (app *Config) getInvoice(u data.User, plan *data.Plan) (string, error) {
 	return plan.PlanAmountFormatted, nil
+}
+
+func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
+	pdf := gofpdf.New("P", "mm", "Letter", "")
+	pdf.SetMargins(10, 13, 10)
+
+	importer := gofpdi.NewImporter()
+
+	time.Sleep(5 * time.Second)
+
+	t := importer.ImportPage(pdf, "./pdf/manual.pdf", 1, "/MediaBox")
+	pdf.AddPage()
+
+	importer.UseImportedTemplate(pdf, t, 0, 0, 215.9, 0)
+
+	pdf.SetX(75)
+	pdf.SetY(150)
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
+	pdf.Ln(5)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", u.Email), "", "C", false)
+
+	return pdf
 }
