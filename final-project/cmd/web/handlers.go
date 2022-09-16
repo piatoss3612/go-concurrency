55a5cb50ef69c560dccc63cd5259c0284cfea405
Hyohwak Lee <crewe1746@naver.com>
2022-09-16 15:13:14 +0900

    Subscription service - generate and send invoice attached to mail

--- final-project/cmd/web/handlers.go
+++ final-project/cmd/web/handlers.go
@@ -192,8 +192,25 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// generate invoice
+	app.Wait.Add(1)
 
-	// send an email with the invoice attached
+	go func() {
+		defer app.Wait.Done()
+
+		invoice, err := app.getInvoice(user, plan)
+		if err != nil {
+			app.ErrorChan <- err
+		}
+
+		msg := Message{
+			To:       user.Email,
+			Subject:  "Your invoice",
+			Data:     invoice,
+			Template: "invoice",
+		}
+
+		app.sendEmail(msg)
+	}()
 
 	// generate a menual
 
@@ -202,4 +219,8 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// subscribe the user to an account
 
 	// redirect
+}
+
+func (app *Config) getInvoice(u data.User, plan *data.Plan) (string, error) {
+	return plan.PlanAmountFormatted, nil
 }
