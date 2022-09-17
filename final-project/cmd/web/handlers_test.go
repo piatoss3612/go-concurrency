794e691d5779e660fd3186c9986fd801c45d0fe5
Hyohwak Lee <crewe1746@naver.com>
2022-09-17 20:26:14 +0900

    Testing - test handlers using concurrency

--- final-project/cmd/web/handlers_test.go
+++ final-project/cmd/web/handlers_test.go
@@ -97,3 +97,27 @@ func TestConfigPostLoginPage(t *testing.T) {
 		t.Error("did not find user id in session")
 	}
 }
+
+func TestConfigSubscribeToPlan(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/subscribe?id=1", nil)
+	ctx := getCtx(req)
+	req = req.WithContext(ctx)
+
+	testApp.Session.Put(ctx, "user", data.User{
+		ID:        1,
+		Email:     "admin@example.com",
+		FirstName: "Admin",
+		LastName:  "User",
+		Active:    1,
+	})
+
+	handler := http.HandlerFunc(testApp.SubscribeToPlan)
+	handler.ServeHTTP(rr, req)
+
+	testApp.Wait.Wait()
+
+	if rr.Code != http.StatusSeeOther {
+		t.Errorf("expected status code of StatusSeeOther, but got %d", rr.Code)
+	}
+}
